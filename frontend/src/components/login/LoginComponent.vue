d2e65ebe356f2c9be17d022515dfef05d0318d30
henstjn <jan.van.der.henst@gmail.com>
2022-10-05 12:43:52 +0200

    begin login

---
+++ frontend/src/components/login/LoginComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="h-screen p-8 flex justify-around">
+    <div class="bg-sky-500 w-1/3 p-8 rounded-2xl">
+      <div class="mb-16">
+        <h3 class="text-2xl text-white font-bold mb-16">SmartShip</h3>
+        <h2 class="text-4xl text-white font-bold mb-4">Start your journey and enable a sustainable maritime future</h2>
+        <p class="text-slate-100 text-base">
+          At Smart-Ship, we make force-feedback control levers for the maritime industry.
+          By bridging the gap in information transfer we aim to enable a sustainable maritime future
+          with regard to people, equipment and the environment.
+        </p>
+      </div>
+
+      <div>
+        <div class="block p-5 max-w bg-sky-700 rounded-xl">
+          <p class="font-normal text-slate-200 mb-2">
+            Our maritime control levers enable operators to sail environmentally friendly,
+            by transferring the vessel’s optimal speed, acceleration rate and rate of turn,
+            without requiring visual feedback.
+          </p>
+        </div>
+      </div>
+
+    </div>
+    <div class="w-1/3 p-8">
+      <h3 class="text-2xl text-slate-800 font-bold">Sign up</h3>
+      <p class="text-md text-slate-500 mb-4">Login to get started</p>
+      <form>
+        <div class="mb-6">
+          <label for="email" class="block mb-1 text-sm font-medium text-slate-500">E-mail</label>
+          <input type="email" id="email" class="bg-white border border-2 border-slate-200 text-slate-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+        </div>
+        <div class="mb-8">
+          <label for="password" class="block mb-1 text-sm font-medium text-slate-500">Password</label>
+          <input type="password" id="password" class="bg-white border border-2 border-slate-200 text-slate-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+        </div>
+        <button type="submit" class="text-white bg-sky-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Login</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from "vue-router";
 import DashboardComponent from "@/components/dashboard/DashboardComponent";
 import DashboardIndex from "@/components/dashboard/DashboardIndex";
 import UnknownRoute from "@/components/UnknownRoute";
+import LoginComponent from "@/components/login/LoginComponent";
 
 const routes = [
     {path: "/", component: UnknownRoute},
@@ -10,6 +11,7 @@ const routes = [
             {path: "/dashboard/:pathMatch(.*)", component: UnknownRoute}
         ]
     },
+    {path: "/login", component: LoginComponent},
     {path: "/:pathMatch(.*)", component: UnknownRoute},
 ];
 
