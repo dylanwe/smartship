86bd5c3794b790d4b1f0e3618d5cc7ae2926c85a
Jesaja Pavlović <jesaja.pavlovic@hva.nl>
2022-10-09 13:23:41 +0200

    add temp test widget

---
+++ frontend/src/components/dashboard/widgets/WidgetTest.vue
@@ -0,0 +1,18 @@
+<template >
+    <div class="bg-red-400">
+        {{title}}
+
+    </div>
+</template>
+<script>
+export default {
+    name:"WidgetTest",
+    props:{
+        title: String,
+        data: Object
+    }
+}
+</script>
+<style >
+    
+</style>
