2b1fa1b7f6844586b39cc427375a00d2205a07b5
Dusan Los <dusan.los@hva.nl>
2022-10-24 14:12:08 +0200

    Notifications front-end

--- frontend/src/components/notification/NotificationIndex.vue
+++ frontend/src/components/notification/NotificationIndex.vue
@@ -1,16 +1,17 @@
 <template>
-  <div>Notifications</div>
+  <div class="text-4xl font-bold pt-10 pb-5">Notifications</div>
 <div class="bg-white rounded-2xl">
-  <ol class="relative border-l border-gray-200 dark:border-gray-700 py-5 px-9">
-    <li class="space-y-3">
-      <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+  <ol class=" pt-9 pb-9 px-9">
+    <li class="space-y-3 hover:bg-sky-50 rounded-md pb-4">
+
+      <div class="h-2 w-2 bg-sky-500 rounded-full flex-row"></div>
       <h3 class="text-lg font-semibold text-gray-900 dark:text-white">System Info</h3>
+
       <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2023, July 13, 10:30 AM</time>
       <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A overview of your system.</p>
       <span class="bg-sky-200 text-sky-800 text-xs font-inter mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-200 dark:text-blue-800">Info</span>
     </li>
-    <li class="space-y-3">
-      <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+    <li class="space-y-3 hover:bg-sky-50 rounded-md">
       <h3 class="text-lg font-semibold text-gray-900 dark:text-white">System Info</h3>
       <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2023, July 13, 10:30 AM</time>
       <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">A overview of your system.</p>
