46b6794f2c93ae33bb2c4ee2eb5871336ab15be5
Dusan Los <dusan.los@hva.nl>
2022-11-22 10:19:34 +0100

    Notification back end

---
+++ backend/src/main/java/com/smartship/backend/app/rest/NotificationController.java
@@ -0,0 +1,16 @@
+package com.smartship.backend.app.rest;
+
+import com.smartship.backend.app.repositories.NotificationRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/notifications")
+public class NotificationController {
+
+//    private final NotificationRepository notificationRepository;
+//
+//    public NotificationController(NotificationRepository notificationRepository) {
+//        this.notificationRepository = notificationRepository;
+//    }
+}
