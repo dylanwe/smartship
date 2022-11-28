17753905e223490d654e832abfa45b6964c6ee22
Jesaja Pavlović <jesaja.pavlovic@hva.nl>
2022-11-28 14:01:05 +0100

    add init dashboard

--- backend/src/main/java/com/smartship/backend/app/BackendApplication.java
+++ backend/src/main/java/com/smartship/backend/app/BackendApplication.java
@@ -1,12 +1,11 @@
 package com.smartship.backend.app;
 
-import com.smartship.backend.app.models.NotificationSetting;
+import com.smartship.backend.app.models.*;
 import com.smartship.backend.app.exceptions.NotAcceptableException;
-import com.smartship.backend.app.models.Dashboard;
-import com.smartship.backend.app.models.User;
 import com.smartship.backend.app.repositories.NotificationSettingRepository;
 import com.smartship.backend.app.repositories.DashboardRepository;
 import com.smartship.backend.app.repositories.UserRepository;
+import com.smartship.backend.app.repositories.WidgetRepository;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,6 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.transaction.Transactional;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @SpringBootApplication
@@ -23,13 +24,16 @@ public class BackendApplication implements CommandLineRunner {
     private final UserRepository userRepository;
     private final NotificationSettingRepository notificationSettingRepository;
     private final DashboardRepository dashboardRepository;
+    private final WidgetRepository widgetRepository;
 
     @Autowired
     public BackendApplication(UserRepository userRepository, DashboardRepository dashboardRepository,
-                              NotificationSettingRepository notificationSettingRepository) {
+                              NotificationSettingRepository notificationSettingRepository, WidgetRepository widgetRepository) {
         this.userRepository = userRepository;
         this.notificationSettingRepository = notificationSettingRepository;
         this.dashboardRepository = dashboardRepository;
+        this.widgetRepository = widgetRepository;
+
     }
 
     public static void main(String[] args) {
@@ -90,8 +94,30 @@ public class BackendApplication implements CommandLineRunner {
     private void createInitialDashboards() {
         if (dashboardRepository.findAll().size() > 0) return;
 
-        dashboardRepository.save(new Dashboard(userRepository.findAll().get(0)));
 
+        Ship shipOne = new Ship("07202515-a483-464c-b704-5671f104044b", "Ship 1");
+
+
+       Widget widget = new Widget("<", "Engine Temperatures", "WidgetTemperature", 2, 2, 2, 2, 2, 2);
+        widgetRepository.save(widget);
+
+       Sensor sensorEngineOneTemp = new Sensor("bb7baec4-c049-45c5-81ce-2715801e6bff",
+                "Engine 1 Temperature", Sensor.GROUP.Motor, Sensor.TYPE.Temperature, "Celsius");
+
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");
+        LocalDateTime dateTime = LocalDateTime.parse("13:10:00 15/07/2022", formatter);
+
+       SensorData shipOneSensorData = new SensorData(67.68, dateTime, sensorEngineOneTemp, shipOne);
+
+        Dashboard dashboard = new Dashboard(userRepository.findAll().get(0));
+        DashboardItem dashboardItem = new DashboardItem(1, 3, 2, 2, widget);
+
+
+        dashboardRepository.save(dashboard);
+        dashboard.addToLayout(dashboardItem);
+
+        System.out.println("Added a initial dashboard");
 
     }
 }
