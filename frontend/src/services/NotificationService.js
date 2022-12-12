c9e748b1601a88bc2e8d24900f013aeddc190274
Dusan Los <dusan.los@hva.nl>
2022-12-12 19:04:56 +0100

    Notification back end

--- frontend/src/services/NotificationService.js
+++ frontend/src/services/NotificationService.js
@@ -1,12 +1,15 @@
-import { Service, Autowired } from "vue-class-component";
 import Notification from "@/models/notifications/Notification";
-import * as NotificationRepository from "backend/src/main/java/com/smartship/backend/app/repositories/NotificationRepository.java";
+import NotificationRepository from "backend/src/main/java/com/smartship/backend/app/repositories/NotificationRepository.java";
+import { Service, Autowired } from "vue-class-component";
 
 @Service
 export default class NotificationService {
     @Autowired
-    private repository: NotificationRepository;
+    repository: NotificationRepository;
 
+    constructor() {
+        this.repository = new NotificationRepository();
+    }
     public findByMessageContainsLetters(letters: string): Notification[] {
         return this.repository.findByMessageContainsLetters(letters);
     }
