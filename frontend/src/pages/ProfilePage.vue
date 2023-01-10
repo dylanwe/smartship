6681bd0c2b636e9338b3016791c04ac668de6756
Lisanne Lin <lisane.lin@hva.nl>
2023-01-10 21:56:40 +0100

    notification from profile page goes to notification page

--- frontend/src/pages/ProfilePage.vue
+++ frontend/src/pages/ProfilePage.vue
@@ -81,19 +81,22 @@
 
         </div>
         <ul>
-          <li class="flex items-center border-b border-neutral-200 py-2" v-for="notification in recentNotifications"
-              :key="notification.id">
-            <svg class="text-primary-500 w-6 h-6 stroke-1.5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none"
-                 viewBox="0 0 24 24"
-                 strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round"
-                    d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"/>
-            </svg>
-            <div>
-              <h4 class="text-lg font-bold">{{ notification.title }}</h4>
-              <p class="text-neutral-700">{{ notification.desc }}</p>
-            </div>
-          </li>
+          <router-link to="/dashboard/notifications">
+            <li class="flex items-center border-b border-neutral-200 py-2 hover:underline"
+                v-for="notification in recentNotifications"
+                :key="notification.id">
+              <svg class="text-primary-500 w-6 h-6 stroke-1.5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none"
+                   viewBox="0 0 24 24"
+                   strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round"
+                      d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"/>
+              </svg>
+              <div>
+                <h4 class="text-lg font-bold">{{ notification.title }}</h4>
+                <p class="text-neutral-700">{{ notification.desc }}</p>
+              </div>
+            </li>
+          </router-link>
         </ul>
       </div>
     </div>
