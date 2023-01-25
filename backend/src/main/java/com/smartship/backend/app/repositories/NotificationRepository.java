ee5d50da9c4913ba92381e68890362609a15cf04
Dusan Los <dusan.los@hva.nl>
2023-01-25 01:13:21 +0100

    BE tests

--- backend/src/main/java/com/smartship/backend/app/repositories/NotificationRepository.java
+++ backend/src/main/java/com/smartship/backend/app/repositories/NotificationRepository.java
@@ -5,6 +5,7 @@ import com.smartship.backend.app.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -12,6 +13,8 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     List<Notification> findTop2ByUserAndReadNotificationIsFalseOrderByNotificationDateTimeAsc(User user);
 
     List<Notification> findAllByUserAndReadNotificationIsFalse(User user);
+
+    List<Notification> findAllByUserIdAndFromToday(Long userId, LocalDateTime date);
 
     List<Notification> findByUserId(Long userId);
 }
